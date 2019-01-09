c559b15a88cfd0769c28499905fe3b4b55901da0
Kerri Harrington <kh302@bath.ac.uk>
2019-01-09 17:37:38 +0000

    Changed zernike_coefficient merit function
    Changed zernike_coefficient merit function

--- beam_profiling.py
+++ beam_profiling.py
@@ -22,6 +22,8 @@ import nplab
 from nplab.utils.array_with_attrs import ArrayWithAttrs
 from measure_orders import POIManager, sum_rois
 
+
+
 def beam_profile_on_SLM(slm, cam, spot, N, overlap=0.0):
     """Scan a spot over the SLM, recording intensities as we go"""
     intensities = np.zeros((N, N))
@@ -125,27 +127,57 @@ def measure_modes(slm, snapshot_fn, spot, N, dz=1, **kwargs):
         print
     return out
     
-def optimise_aberration_correction(slm, cam, zernike_coefficients, merit_function, dz=1, modes=None):
+def optimise_aberration_correction(slm, cam, merit_function, dz=1, modes=None):
     """Tweak the Zernike coefficients incrementally to optimise them."""
 
     def test_coefficients(coefficients):
         slm.zernike_coefficients = coefficients
         return merit_function()
     
-    coefficients = np.array(zernike_coefficients)
+    coefficients = np.array(slm.zernike_coefficients)
     for i in (range(len(coefficients)) if modes is None else modes):
         values = np.array([-1,0,1]) * dz + coefficients[i]
         merits = np.zeros(values.shape[0])
         for j, v in enumerate(values):
             coefficients[i] = v
             merits[j] = test_coefficients(coefficients)
-        if merits.argmax() == 0:
-            pass # might want to go round for another try?
-        elif merits.argmax() == len(merits) - 1:
-            pass
+        # Keep taking more points until we have a maximum
+        while merits.argmax() == 0 or merits.argmax() == len(merits) - 1:
+            if merits.argmax() == 0:
+                values = np.append([values[0] - dz], values)
+                coefficients[i] = values[0]
+                merits = np.append([test_coefficients(coefficients)], merits)
+            else:
+                values = np.append(values, [values[0] + dz])
+                coefficients[i] = values[-1]
+                merits = np.append(merits, [test_coefficients(coefficients)])      
         coefficients[i] = values[merits.argmax()]
         test_coefficients(coefficients)
     return coefficients
+
+def sweep_zernike_mode(slm, cam, merit_function, dz=np.linspace(-1,1,11), mode=1):
+    """Tweak the Zernike coefficients incrementally to optimise them."""
+
+    def test_coefficients(coefficients):
+        slm.zernike_coefficients = coefficients
+        return merit_function()
+    
+    starting_coefficients = np.array(slm.zernike_coefficients)
+    z_values = np.array(dz) + starting_coefficients[mode]
+    merit_values =  ArrayWithAttrs(np.zeros_like(z_values))
+    merit_values.attrs['zernike_coefficients'] = starting_coefficients
+    merit_values.attrs['mode_swept'] = mode
+    merit_values.attrs['z_values'] = z_values
+    for i, z in enumerate(z_values):
+        coefficients = starting_coefficients.copy()
+        coefficients[mode] = z
+        merit_values[i] = test_coefficients(coefficients)
+    slm.zernike_coefficients = starting_coefficients
+    dset = nplab.current_datafile().create_dataset("zernike_sweep_%d",data=merit_values)
+    plt.figure()
+    plt.plot(z_values, merit_values, 'o')
+    plt.suptitle("Sweeping mode {}".format(mode))
+    return dset
     
     
 def calibrate_hdr():
@@ -176,6 +208,8 @@ def focus_stack(N, dz, snap=None):
     zernike_coefficients = slm.zernike_coefficients
     for i, d in enumerate(np.linspace(-dz,dz,N)):
         z = zernike_coefficients.copy()
+        #z = zernike_coefficients #attempt to fix1
+        #z = slm.zernike_coefficients.copy() #attempt to fix2
         z[1] += d
         slm.zernike_coefficients = z
         #focus_stack[i,:,:] = snap()
@@ -230,97 +264,6 @@ if __name__ == '__main__':
         
     df = nplab.current_datafile()
     
-#    test_spot = [200,-200,0,100]
-#    distance = 3000e3
-#    slm.make_spots([test_spot + [0,0,0.075,0]])
-#    slm.update_gaussian_to_tophat(1900,1, distance=distance)
-#    cam.show_gui()
-#    time.sleep(1)
-#    pause()
-#    zernike_coefficients = np.zeros(12)
-#    slm.zernike_coefficients = zernike_coefficients
-#    print('Sequential Shack Hartmann')
-#    res = sequential_shack_hartmann(slm, snap, test_spot, 10, overlap=0.5)
-#    plot_shack_hartmann(res)
-#    pause()
-#    
-#    modes = measure_modes(slm, snap, [20,10,2050,1], 5, overlap=0)
-#    flat = modes[12]
-#    f, axes = plt.subplots(3,4)
-#    axes_flat = [axes[i,j] for i in range(3) for j in range(4)]
-#    for m, ax in zip(modes[:12], axes_flat):
-#        plot_sh_shifts(ax, m, discard_edges=1)
-#    pause()
-#    
-#    # Analysis of the intensity distribution on the SLM
-#    # NB slmsize is set at 6.9mm for the CC SLM
-#    slm_size = 6.9
-#    initial_r = 1.9
-#    N = res.shape[0]
-#    x = (np.arange(N)-(N-1)/2.0)/N * slm_size #centres of apertures
-#    gaussian = np.exp(-x**2/initial_r**2)
-#    gaussian /= np.mean(gaussian)
-#    f, axes = plt.subplots(1,2)
-#    # plot the data and a Gaussian fit
-#    for data, ax in zip([res[:,:,2], res[:,:,2].T], axes):
-#        for i in range(N):
-#            ax.plot(x, data[:,i], '+', color=plt.cm.gist_rainbow(float(i)/N))
-#            m = np.polyfit(gaussian, data[:,i], 1)
-#            ax.plot(x, gaussian*m[0]+m[1], '-', color=plt.cm.gist_rainbow(float(i)/N))
-#    # plot the data against the Gaussian
-#    f, axes = plt.subplots(1,2)
-#    for data, ax in zip([res[:,:,2], res[:,:,2].T], axes):
-#        for i in range(N):
-#            ax.plot(gaussian, data[:,i], '-+', color=plt.cm.gist_rainbow(float(i)/N))
-#
-#    # plot the data vs r
-#    r = np.sqrt(x[:,np.newaxis]**2 + x[np.newaxis,:]**2)
-#    f, ax = plt.subplots(1,1)
-#    ax.plot(r.flatten(), res[:,:,2].flatten(), '.')
-#
-#    # find the centroid
-#    plt.figure()
-#    s=slm_size/2.0
-#    plt.imshow(res[:,:,2],extent=(-s,s,-s,s))
-#    for thresh in [0.0,0.1,0.2,0.5,0.9]:
-#        I = res[:,:,2].copy()
-#        I /= np.max(I)
-#        I -= thresh
-#        I[I<0] = 0
-#        cx = np.sum(x[:,np.newaxis]*I)/np.sum(I)
-#        cy = np.sum(x[np.newaxis,:]*I)/np.sum(I)
-#        hide = plt.plot(cy,cx,'+')
-#        print "found centroid at {}, {} with threshold {}".format(cx, cy, thresh)
-#
-#    # pick the right values for cx and cy...
-#
-#    # plot the data vs r
-#    r = np.sqrt((x[:,np.newaxis]-cx)**2 + (x[np.newaxis,:]-cy)**2)
-#    f, ax = plt.subplots(1,1)
-#    ax.plot(r.flatten(), res[:,:,2].flatten(), '.')
-#    
-#    radii = r.flatten()
-#    I = res[:,:,2].flatten()/I.max()*10
-#    from scipy.interpolate import UnivariateSpline
-#    #spl = UnivariateSpline(np.concatenate([radii,-radii]), np.tile(I,2))
-#    rr = np.linspace(0,slm_size/np.sqrt(2),100)
-#    #plt.plot(rr,spl(rr))
-#    plt.plot(radii,I,'.')
-#    pause()
-#    
-#    slm.centre=(cx,cy)
-#    slm.radial_blaze_function = np.ones(384)
-#    inner_edge_i = 1500//18 #bad
-#    sd = 400/18.0
-#    inner_edge_i = 1000//18 #good
-#    sd = 1000/18.0
-#    nrest = 384 - inner_edge_i
-#    radial_blaze_function = np.concatenate([np.ones(inner_edge_i),
-#                                        np.exp(-(np.arange(nrest))**2/2.0/sd**2)])
-#    slm.radial_blaze_function = radial_blaze_function #radial blaze function moves spot - change in centre?
-#    cam.show_gui()
-#    time.sleep(1)
-#    pause()
 
 
 """
@@ -405,6 +348,8 @@ inner_edge_i = 1500//18 #bad
 sd = 400/18.0
 inner_edge_i = 1000//18 #good
 sd = 1000/18.0
+inner_edge_i = 800//18 #good
+sd = 1400/18.0
 nrest = 384 - inner_edge_i
 radial_blaze_function = np.concatenate([np.ones(inner_edge_i),
                                         np.exp(-(np.arange(nrest))**2/2.0/sd**2)])
@@ -428,7 +373,7 @@ slm.zernike_coefficients = zernike_coefficients
 #cam.exposure=-2
 def brightest_hdr():
     hdr = snap()
-    for i in range(4):
+    for i in range(3):
         hdr += snap()
     hdr /= 5
     return np.max(scipy.ndimage.uniform_filter(hdr, 17))
@@ -448,21 +393,31 @@ def beam_sd():
     #cam.color_image()
     time.sleep(0.1)
     hdr = snap()
+    # Apply a basic threshold to get rid of some background
+    threshold = np.max(hdr) * 0.5
+    hdr[hdr < threshold] = threshold
+    hdr -= threshold
+    
+    # find the centroid
     x = np.mean(hdr * np.arange(hdr.shape[0])[:,np.newaxis])/np.mean(hdr)
     y = np.mean(hdr * np.arange(hdr.shape[1])[np.newaxis,:])/np.mean(hdr)
+    
+    # find the second moment, about the centroid
     dx2 = np.mean(hdr * ((np.arange(hdr.shape[0])-x)**2)[:,np.newaxis])/np.mean(hdr)
     dy2 = np.mean(hdr * ((np.arange(hdr.shape[1])-y)**2)[np.newaxis,:])/np.mean(hdr)
     sd = np.sqrt(dx2+dy2)
     return 1/sd
+    
 def average_fn(f, n):
     return lambda: np.mean([f() for i in range(n)])
 merit_function = lambda: np.mean([beam_sd() for i in range(3)])
 
 # Start by autofocusing (optimising mode 2)
-zernike_coefficients = optimise_aberration_correction(slm, cam, zernike_coefficients, beam_sd, dz=0.5, modes=[1])
+zernike_coefficients = optimise_aberration_correction(slm, cam, zernike_coefficients, brightest_hdr, dz=0.5, modes=[1])
 zernike_coefficients = optimise_aberration_correction(slm, cam, zernike_coefficients, beam_sd, dz=0.1, modes=[1])
 zernike_coefficients = optimise_aberration_correction(slm, cam, zernike_coefficients, beam_sd, dz=0.3, modes=[0,1,2])
-for dz in [0.5,0.35,0.3,0.2,0.15,0.1,0.07,0.05,0.01]:
+zernike_coefficients = slm.zernike_coefficients
+for dz in [0.5,0.35,0.3,0.2,0.15,0.11]:
     print "step size: {}".format(dz)
     zernike_coefficients = optimise_aberration_correction(slm, cam, zernike_coefficients, beam_sd, dz=dz)
 zernike_coefficients = optimise_aberration_correction(slm, cam, zernike_coefficients, average_fn(beam_sd,3), dz=0.1)
@@ -480,10 +435,10 @@ nplab.current_datafile().create_dataset("spot_image_%d",data=cam.color_image(),a
 # Really thorough optimisation of defocus
 # Start with a visible, nicely-focused spot
 #test_spot = [20,-10,0,1];
-slm.make_spots([test_spot + [0,0,0.2,0]])
+slm.make_spots([test_spot[:4] + [0,0,0.2,0]])
 slm.update_gaussian_to_tophat(1900,1, distance=distance)
 #focus_stack(50,5, snap=snap)
-focus_stack(50,5, snap=snap)
+focus_stack(50,5, snap=snap) #focus_stack changes zcs from set
 
 """
 """
@@ -516,5 +471,132 @@ f, axes = plt.subplots(1,2)
 axes[0].imshow(xsection,aspect='auto',extent = (0,640,rs.max(), rs.min()))
 axes[1].imshow(ysection,aspect='auto',extent = (0,480,rs.max(), rs.min()))
 
+#Scanning values of tophat
+thimages = []
+lineprofiles = []
+for i in range(15, 27, 2):
+    thnum = float(i)/10
+    slm.update_gaussian_to_tophat(thnum, 2)
+    images = []
+    for j in range (5):
+        images.append(snap())
+    averaged = np.mean(images, axis=0)
+    thimages.append(averaged)
+    img = averaged
+    lineprofiles.append(img[:,img.shape[1]//2])
+    time.sleep(0.1)
+
+#Plot the line profiles
+for i in range(0, len(lineprofiles)-1):
+    plt.plot(lineprofiles[i])
+
+plt.xlabel('Pixels')
+plt.ylabel('Intensity (arb)')
+plt.show()
+
+##Changing radial function
+
+for k in range(19, 27, 2):
+    thnum = float(k)/10
     
+    thimages = []
+    lineprofiles = []
+    for i in range(1500, 2000, 100):
+        inner_edge_i = 800//18 #good
+        sdtest = i/18.0
+        sd = sdtest
+        nrest = 384 - inner_edge_i
+        radial_blaze_function = np.concatenate([np.ones(inner_edge_i),
+                                        np.exp(-(np.arange(nrest))**2/2.0/sd**2)])
+        slm.radial_blaze_function = radial_blaze_function
+        slm.make_spots([test_spot[:4] + [0,0,1,0]])
+        slm.update_gaussian_to_tophat(thnum, 2)
+        images = []
+        for j in range (5):
+            images.append(snap())
+        averaged = np.mean(images, axis=0)
+        thimages.append(averaged)
+        img = averaged
+        lineprofiles.append(img[img.shape[0]//2,:])
+
+
+    #Plot the line profiles
+    for i in range(0, len(lineprofiles)-1):
+        plt.plot(lineprofiles[i])
+
+    plt.title(thnum)
+    plt.xlabel('Pixels')
+    plt.ylabel('Intensity (arb)')
+    plt.show()
+
+##images
+for k in range(21, 25, 1):
+    thnum = float(k)/10
+    
+    thimages = []
+    lineprofiles = []
+    for i in range(1000, 2500, 200):
+        inner_edge_i = 800//18 #good
+        sdtest = i/18.0
+        sd = sdtest
+        nrest = 384 - inner_edge_i
+        radial_blaze_function = np.concatenate([np.ones(inner_edge_i),
+                                        np.exp(-(np.arange(nrest))**2/2.0/sd**2)])
+        slm.radial_blaze_function = radial_blaze_function
+        slm.make_spots([test_spot[:4] + [0,0,1,0]])
+        slm.update_gaussian_to_tophat(thnum, 2)
+        images = []
+        for j in range (5):
+            images.append(snap())
+        averaged = np.mean(images, axis=0)
+        thimages.append(averaged)
+        img = averaged
+        plt.title(thnum)
+        plt.plot(img[img.shape[0]//2,:])
+        print(i)
+        plt.imshow(img)
+        plt.show()
+
+##A pick: 2.5 1600
+inner_edge_i = 800//18 #good
+sdtest = 1600/18.0
+sd = sdtest
+nrest = 384 - inner_edge_i
+radial_blaze_function = np.concatenate([np.ones(inner_edge_i),
+                                        np.exp(-(np.arange(nrest))**2/2.0/sd**2)])
+slm.radial_blaze_function = radial_blaze_function
+slm.make_spots([test_spot[:4] + [0,0,1,0]])
+slm.update_gaussian_to_tophat(2.5, 2)
+
+##
+
+for k in range(22, 26, 1):
+    thnum = float(k)/10
+    slm.zernike_coefficients = np.zeros(12)
+    thimages = []
+    lineprofiles = []
+    for i in range(800, 3000, 200):
+        inner_edge_i = 800//18 #good
+        sdtest = i/18.0
+        sd = sdtest
+        nrest = 384 - inner_edge_i
+        radial_blaze_function = np.concatenate([np.ones(inner_edge_i),
+                                        np.exp(-(np.arange(nrest))**2/2.0/sd**2)])
+        slm.radial_blaze_function = radial_blaze_function
+        slm.make_spots([test_spot[:4] + [0,0,1,0]])
+        slm.update_gaussian_to_tophat(thnum, 2)
+        images = []
+        for j in range (5):
+            images.append(snap())
+        averaged = np.mean(images, axis=0)
+        thimages.append(averaged)
+        img = averaged
+        plt.title(thnum)
+        plt.plot(img[img.shape[0]//2,:])
+        print(i)
+        plt.imshow(img)
+        plt.show()
+        
+        focus_stack(50,5, snap=snap)
+        plt.show()
 """
